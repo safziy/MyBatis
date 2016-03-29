2240b19e515ff67c614ff23780d97a8b73d2fa38
g <safziy@foxmail.com>
2016-03-29 20:56:28 +0800

    Commit

--- src/test/java/com/safziy/dao/test/ClassInfoTest.java
+++ src/test/java/com/safziy/dao/test/ClassInfoTest.java
@@ -18,5 +18,17 @@ public class ClassInfoTest extends BaseTest {
 		session.commit();
 		session.close();
 	}
+	
+	@Test
+	public void testLoadById2() {
+		// 默认不是自动提交的 需要自动提交的话 sessionFactory.openSession(true)
+		SqlSession session = sessionFactory.openSession();
+
+		String statement = "com.safziy.bean.classinfo.mapper.loadById2";
+		ClassInfo classInfo = session.selectOne(statement, 1);
+		System.out.println("ClassInfoTest testLoadById() == " + classInfo);
+		session.commit();
+		session.close();
+	}
 
 }
